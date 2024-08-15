0ecf9ac6b616c84b968e0dae531f30f668209e1b
Chase Hawley <hawleych@msu.edu>
2024-08-15 10:29:14 -0400

    Added custom effect context class

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraAbilityTypes.h"
+
+AuraAbilityTypes::AuraAbilityTypes()
+{
+}
+
+AuraAbilityTypes::~AuraAbilityTypes()
+{
+}
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return true;
+}
---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	virtual UScriptStruct* GetScriptStruct() { return FGameplayEffectContext::StaticStruct(); }
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
+
+/**
+ * 
+ */
+class AURA_API AuraAbilityTypes
+{
+public:
+	AuraAbilityTypes();
+	~AuraAbilityTypes();
+};
