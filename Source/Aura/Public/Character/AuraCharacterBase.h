37df77a9e61e7f989c80a34d30528ed35879497e
Chase Hawley <hawleych@msu.edu>
2024-09-26 11:15:58 -0400

    Slingshot goblins now attack player, complete with projectiles, animations, the whole 9

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -47,7 +47,7 @@ protected:
 
 	virtual void InitAbilityActorInfo();
 
-	UPROPERTY(EditAnywhere, Category = "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 	UPROPERTY(EditAnywhere, Category = "Combat")
